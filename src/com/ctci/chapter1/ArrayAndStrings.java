77bd7acc3060f6edc7c9a85b90d728476f56cd13
codeandqa <aditya@anwisys.com>
2020-01-10 00:43:23 -0500

    added UniqueEmail question.

--- src/com/ctci/chapter1/ArrayAndStrings.java
+++ src/com/ctci/chapter1/ArrayAndStrings.java
@@ -1,13 +1,25 @@
 package com.ctci.chapter1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 public class ArrayAndStrings {
 
 	public static void main(String[] args) {
-		boolean output = isUnique2("helaoss");
-		System.out.println(output);
+		String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+		numUniqueEmails(emails);
 	}
 
+	//Given two strings, write a method to 
+    //determine if one string is permutation of another string.
+	
+	
+	
+	
 	// 1. Is Unique: Implement an algorithm to determine if a string
 	// has all unique characters. What if you cannot use additional data structure
 	//
@@ -40,4 +52,34 @@ public class ArrayAndStrings {
 			return false;
 		}
 	}
+	
+		static int lengthOfLongestSubstring(String s) {
+	        int n = s.length(), ans = 0;
+	        Map<Character, Integer> map = new HashMap<>(); // current index of character
+	        // try to extend the range [i, j]
+	        for (int j = 0, i = 0; j < n; j++) {
+	            if (map.containsKey(s.charAt(j))) {
+	                i = Math.max(map.get(s.charAt(j)), i);
+	            }
+	            ans = Math.max(ans, j - i + 1);
+	            map.put(s.charAt(j), j + 1);
+	        }
+	        return ans;
+	    }
+	
+		static int numUniqueEmails(String[] emails) {
+			Set<String> hash_Set = new HashSet<String>(); 
+	        for(String email:emails){
+	            String[] em = email.split("@");
+	            String user = em[0];
+	            if(em[0].contains("+")){
+	                user = user.substring(0,user.indexOf('+'));
+	            }
+	            
+	            user = user.replaceAll(".", "");
+	            hash_Set.add(user+"@"+em[1]);
+	        }
+	        return hash_Set.size();
+			
+	    }
 }
